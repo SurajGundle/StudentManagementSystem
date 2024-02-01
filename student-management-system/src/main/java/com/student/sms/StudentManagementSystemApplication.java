ee7a8c22ffecd5e545ea495334725b659e95d8d0
SurajGundle <shubhamgundle35@gmail.com>
2024-02-01 12:52:08 +0530

    sms

---
+++ student-management-system/src/main/java/com/student/sms/StudentManagementSystemApplication.java
@@ -0,0 +1,40 @@
+package com.student.sms;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.student.sms.entity.Student;
+import com.student.sms.repository.StudentRepository;
+
+@SpringBootApplication
+public class StudentManagementSystemApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentManagementSystemApplication.class, args);
+	}
+	
+	@Autowired
+    private StudentRepository studentRepository;
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		/*
+		 * Student student1=new Student("suraj", "gundle", "sg@gmail.com");
+		 * studentRepository.save(student1);
+		 * 
+		 * Student student2=new Student("shubham", "gundle", "shubham@gmail.com");
+		 * studentRepository.save(student2);
+		 * 
+		 * Student student3=new Student("prasad", "kadam", "ps@gmail.com");
+		 * studentRepository.save(student3);
+		 * 
+		 * Student student4=new Student("gorak", "gawali", "gorak@gmail.com");
+		 * studentRepository.save(student4);
+		 */
+		
+	}
+
+}
